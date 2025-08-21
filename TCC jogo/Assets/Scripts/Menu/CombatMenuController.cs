fc8364d15664a022b7a45426057d2ba886b56f1e
RafaelNunesMuller <rafaelmuller007@icloud.com>
2025-08-21 17:27:45 -0300

    menuEquip

--- TCC jogo/Assets/Scripts/Menu/CombatMenuController.cs
+++ TCC jogo/Assets/Scripts/Menu/CombatMenuController.cs
@@ -3,14 +3,13 @@ using UnityEngine.UI;
 
 public class CombatMenuController : MonoBehaviour
 {
-    public Text[] opcoesMenu;           // As opções do menu (Text ou TMP_Text)
+    public Button[] opcoesMenu;           // As opções do menu (Text ou TMP_Text)
     public RectTransform cursor;        // O cursor que será movido
     private int opcaoSelecionada = 0;
 
     void Start()
     {
         AtualizarCursor();
-        AtualizarSelecaoVisual();
     }
 
     void Update()
@@ -34,21 +33,15 @@ public class CombatMenuController : MonoBehaviour
             ExecutarAcao(opcaoSelecionada);
         }
     }
-    void AtualizarSelecaoVisual()
-    {
-        for (int i = 0; i < opcoesMenu.Length; i++)
-        {
-            opcoesMenu[i].color = Color.white;
-        }
-    }
+    
 
     void AtualizarCursor()
     {
         // Reposiciona o cursor como filho do botão atual (alinha perfeitamente)
-        cursor.SetParent(opcoesMenu[opcaoSelecionada].transform, false);
+        cursor.SetParent(opcoesMenu[opcaoSelecionada].transform, true);
 
         // Posiciona um pouco à esquerda do texto
-        cursor.anchoredPosition = new Vector2(200, 0); // distância da esquerda
+        cursor.anchoredPosition = new Vector2(195, -15); // distância da esquerda
     }
 
 
