275967f48f019ca43cf3c7f0105f9aea215c48d9
RafaelNunesMuller <rafaelmuller007@icloud.com>
2025-11-06 15:32:19 -0300

    baus e transições de telas

--- TCC jogo/Assets/Scenes/Loja (Compras)/Script/PainelInfo.cs
+++ TCC jogo/Assets/Scenes/Loja (Compras)/Script/PainelInfo.cs
@@ -21,6 +21,7 @@ public class ItemInfoUI : MonoBehaviour
     public GameObject Vendedor;
     public Button Okay;
     public int itemPrice;
+    public GameObject player;
 
     public static Inventario instance;
     public static CoinManager Coins;
